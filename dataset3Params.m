118db21f7be858e92a72f1d99064f5023209bdb3
RussellM2020 <russellm@berkeley.edu>
2017-01-14 15:09:45 +0300

    Update dataset3Params.m

--- dataset3Params.m
+++ dataset3Params.m
@@ -10,6 +10,21 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 % You need to return the following variables correctly.
 C = 1;
 sigma = 0.3;
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+
+% =========================================================================
 possible_vals = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
 l=length(possible_vals);
 m=length(yval);
@@ -33,18 +48,5 @@ sigma = possible_vals(column);
 
 
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the optimal C and sigma
-%               learning parameters found using the cross validation set.
-%               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
-%                   predictions = svmPredict(model, Xval);
-%               will return the predictions on the cross validation set.
-%
-%  Note: You can compute the prediction error using 
-%        mean(double(predictions ~= yval))
-%
-
-% =========================================================================
 
 end
